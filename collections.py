b78f84053c9cf885cb29933b96920169f0e45ffd
CyberVoid-dev-Josh <141589193+CyberVoid-dev-Josh@users.noreply.github.com>
2023-08-18 11:00:26 -0600

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,40 @@
+# Lista
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+# Valores lista
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# Actualizacion de la lista
+myFruitList[2] = "orange"
+print(myFruitList)
+
+# Tupla = Lista
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+# Valores de tupla
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Diccionario
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+# Imprime el diccionario
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+# Acceso a los nombres en diccionario
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
---
+++ for-loop.py
@@ -0,0 +1,5 @@
+print("Count to 10!")
+
+# bucle numros x del 0 al 10 va aunmentando el rango
+for x in range (0, 11):
+    print(x)
